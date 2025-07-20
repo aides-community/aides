944b1c955c9865bbc6f6dfc05ac2f8f8b80fc68e
Automated Update <aides-helper-bot@noreply.altlinux.space>
2025-07-20 15:39:32 +0000

    Automated update: 2025-07-20 15:39:32

--- vk-messenger/.stapler/_utils.py
+++ vk-messenger/.stapler/_utils.py
@@ -1,10 +1,10 @@
 import subprocess
 
 def get_field(field: str) -> str:
-    return subprocess.check_output(['alr-spec', 'get-field', field]).decode().strip()
+    return subprocess.check_output(['stplr-spec', 'get-field', field]).decode().strip()
 
 def set_field(field: str, value: str):
-    subprocess.check_call(['alr-spec', 'set-field', str(field), str(value)])
+    subprocess.check_call(['stplr-spec', 'set-field', str(field), str(value)])
 
 def get_etag_lastmod(r):
     return r.headers.get('ETag', '').strip('"'), r.headers.get('Last-Modified', '')
