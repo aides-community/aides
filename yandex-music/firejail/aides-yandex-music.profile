af28c81a5412542a0ab5dc861814400d2a9fb0c7
Automated Update <aides-helper-bot@noreply.altlinux.space>
2025-09-07 16:06:51 +0000

    Automated update: 2025-09-07 16:06:51

---
+++ yandex-music/firejail/aides-yandex-music.profile
@@ -0,0 +1,7 @@
+noblacklist ${HOME}/.config/YandexMusic
+mkdir ${HOME}/.config/YandexMusic
+whitelist ${HOME}/.config/YandexMusic
+
+ignore dbus-user none
+
+include electron.profile
---
+++ yandex-music/yandex-music.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Issue: https://github.com/electron/electron/issues/46538
+# Hack:  https://github.com/cypress-io/cypress/issues/32361#issuecomment-3228470484
+/opt/YandexMusic/yandexmusic --gtk-version=3
